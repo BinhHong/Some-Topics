6ed6ecfdfaaeedfbeb2c9d5a794139208b94b4d3
Binh Hong Ngoc <binh.hongngoc@gmail.com>
2024-03-19 23:39:49 +0100

    add Python folder

---
+++ Python/function.py
@@ -0,0 +1,12 @@
+"harry boc cuc".title()
+"harry boc cuc".capitalize()
+"harry boc cuc".upper()
+
+# positional and keyword argument
+def f(a,b=1):
+    return a+b
+
+f(5)
+f(5,2)
+print(f(a=5))
+#f(a=5,2)
